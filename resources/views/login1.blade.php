604afa1c4d3085f2c87cfb226950e8f788e928a4
ferdion janberto <ferdion28@gmail.com>
2017-08-21 13:27:39 +0700

    first commit

---
+++ resources/views/login1.blade.php
@@ -0,0 +1,24 @@
+      <!-- BEGIN LOGIN -->
+        <div class="content">
+            <!-- BEGIN LOGIN FORM -->
+            <form class="login-form" action="index.html" method="post">
+                <h3 class=" green-jungle">Masuk</h3>
+                <h4 class="form-title green-jungle" align="center">Welcome to I-Smartway Dashboard</h4>
+                <div class="alert alert-danger display-hide">
+                    <button class="close" data-close="alert"></button>
+                    <span> Maskukkan Password yang sesuai</span>
+                </div>
+                <div class="form-group">
+                    <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+                    <label class="control-label visible-ie8 visible-ie9">Username</label>
+                    <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Masukkan Username" name="username" /> </div>
+                <div class="form-group">
+                    <label class="control-label visible-ie8 visible-ie9">Password</label>
+                    <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Masukkan Password" name="password" /> </div>
+                <div class="form-actions" align="center">
+                    <button type="submit" class="btn green-jungle uppercase">LOGIN</button>
+                </div>
+            </form>
+            <!-- END LOGIN FORM -->
+        </div>
+        <div class="copyright green-meadow"> 2017 © I-Smartway. Startup Company. </div>
